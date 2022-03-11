2609fdee444ce00a695b2e53d1fb900ae20cf340
Sakshi-Halge <sakshihalge2000@gmail.com>
2022-03-11 19:01:47 +0530

    mongodb url changed

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 var mongoose = require("mongoose");
 mongoose.connect(
-  "mongodb+srv://m001-student:fullstack@123@sandbox.sdinu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  "mongodb+srv://sakshi:hackerrank&*2109@sandbox.4zplg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 );
